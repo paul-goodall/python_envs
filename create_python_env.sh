6899f24d303ff43418e4e70221ff9a91203fa602
paul-goodall <paul.thomas.goodall@gmail.com>
2022-12-15 13:10:06 +1100

    ammending the call scripts

---
+++ create_python_env.sh
@@ -0,0 +1,56 @@
+
+#!/usr/bin/env bash
+
+my_wd=`pwd`
+my_project_name=${PWD##*/}          # to assign to a variable
+my_project_name=${my_project_name:-/}        # to correct for the case where PWD=/
+my_host=`hostname`
+my_host=${my_host/.local/}
+my_envs_dir="${my_wd}/envs"
+my_envs_git_dir="${my_envs_dir}/.git"
+my_host_env="${my_envs_dir}/${my_host}"
+my_python_installs="${my_envs_dir}/python_installs.sh"
+echo $my_wd
+echo $my_project_name
+echo $my_host
+echo $my_envs_dir
+echo $my_host_env
+echo $my_python_installs
+
+exit
+
+# Make the envs dir if it doesn't already exist:
+# It should alrady exist though due to:
+# git clone https://github.com/paul-goodall/python_envs.git
+# wget -O - https://raw.githubusercontent.com/<username>/<project>/<branch>/<path>/<file> | bash
+mkdir -p $my_envs_dir
+
+# remove the git part if it exists:
+if [ -d "$my_envs_git_dir" ];
+then
+  rm -rf $my_envs_git_dir
+fi
+
+
+# Get things up-to-date
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools wheel virtualenv --upgrade
+
+# Create the env if it doesn't already exist:
+if [ ! -d "$my_host_env" ];
+then
+  python3 -m venv $my_host_env
+fi
+
+# activate the venv:
+source ${my_host_env}/bin/activate
+
+# install ipython INSIDE the venv
+python3 -m pip install --upgrade pip
+python3 -m pip install ipython
+python3 -m pip install ipykernel
+
+# install the venv as a Jupyter kernel
+ipython kernel install --user --name=${my_project_name}
+
+bash $my_python_installs
--- go_python_env.sh
+++ go_python_env.sh
@@ -7,6 +7,7 @@ my_project_name=${my_project_name:-/}        # to correct for the case where PWD
 my_host=`hostname`
 my_host=${my_host/.local/}
 my_envs_dir="${my_wd}/envs"
+my_envs_git_dir="${my_envs_dir}/.git"
 my_host_env="${my_envs_dir}/${my_host}"
 my_python_installs="${my_envs_dir}/python_installs.sh"
 echo $my_wd
@@ -16,28 +17,4 @@ echo $my_envs_dir
 echo $my_host_env
 echo $my_python_installs
 
-# Make the envs dir if it doesn't already exist:
-mkdir -p $my_envs_dir
-
-# Get things up-to-date
-python3 -m pip install --upgrade pip
-python3 -m pip install setuptools wheel virtualenv --upgrade
-
-# Create the env if it doesn't already exist:
-if [ ! -d "$my_host_env" ];
-then
-  python3 -m venv $my_host_env
-fi
-
-# activate the venv:
-source ${my_host_env}/bin/activate
-
-# install ipython INSIDE the venv
-python3 -m pip install --upgrade pip
-python3 -m pip install ipython
-python3 -m pip install ipykernel
-
-# install the venv as a Jupyter kernel
-ipython kernel install --user --name=${my_project_name}
-
-bash $my_python_installs
+exit
