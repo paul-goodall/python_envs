c2d2a597f7bbd41476b82f370ae14303a2ac90f9
paul-goodall <paul.thomas.goodall@gmail.com>
2022-12-15 13:45:05 +1100

    ammending run script x6

---
+++ other_useful.sh
@@ -0,0 +1,15 @@
+# NOTES
+
+# show packages installed:
+# pip list
+
+# show packages installe by pip:
+# pip freeze
+
+# uninstall the Venv from Jupyter:
+# jupyter kernelspec list
+# jupyter-kernelspec uninstall _insert_kernel_name_case_sensitive_
+# jupyter kernelspec remove _insert_kernel_name_case_sensitive_
+
+# quit the venv:
+# deactivate
